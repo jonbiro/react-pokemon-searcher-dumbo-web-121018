8bd59b7433dcec1149094fb418a0d92c6b440758
Jonathan Biro <jonnybrx@gmail.com>
2019-02-27 16:25:52 -0500

    click

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  state = {
+    clicked: false
+  };
+
+  toggleImage = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
   render() {
+    let hp = this.props.pokemon.stats.find(name => name.name === "hp").value;
+    let url = !this.state.clicked ? this.props.pokemon.sprites.front : this.props.pokemon.sprites.back
     return (
       <Card>
-        <div>
+        <div onClick={() => this.toggleImage()}>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={url} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp} HP
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,14 +5,24 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
-  render() {
+	state={
+		pokemon: []
+	}
+	
+	componentDidMount() {
+		fetch('http://localhost:3000/pokemon')
+			.then(res=>res.json())
+			.then(pokemon => this.setState({pokemon}))
+	}
+	
+	render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
         <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon} />
         <br />
         <PokemonForm />
       </div>
